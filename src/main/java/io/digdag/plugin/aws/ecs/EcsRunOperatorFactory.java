b2de5a894388cf09deaf9563d14f07e5be278c69
Makoto Yui <myui@apache.org>
2019-02-26 12:21:07 +0900

    Added unit tests

--- src/main/java/io/digdag/plugin/aws/ecs/EcsRunOperatorFactory.java
+++ src/main/java/io/digdag/plugin/aws/ecs/EcsRunOperatorFactory.java
@@ -11,20 +11,14 @@
  */
 package io.digdag.plugin.aws.ecs;
 
-import static java.util.stream.Collectors.toList;
 import io.digdag.client.config.Config;
 import io.digdag.spi.Operator;
 import io.digdag.spi.OperatorContext;
 import io.digdag.spi.OperatorFactory;
 import io.digdag.spi.TaskResult;
-import java.util.List;
 import javax.annotation.Nonnull;
 import com.amazonaws.services.ecs.AmazonECSClient;
-import com.amazonaws.services.ecs.model.PlacementConstraint;
-import com.amazonaws.services.ecs.model.PlacementStrategy;
 import com.amazonaws.services.ecs.model.RunTaskRequest;
-import com.amazonaws.services.ecs.model.TaskOverride;
-import com.amazonaws.services.ecs.model.transform.RunTaskRequestMarshaller;
 
 public final class EcsRunOperatorFactory implements OperatorFactory {
 
@@ -52,21 +46,21 @@ public final class EcsRunOperatorFactory implements OperatorFactory {
           request.getConfig().mergeDefault(request.getConfig().getNestedOrGetEmpty("ecs"));
 
       AmazonECSClient client = getEcsClient();
-      
-      
+
+
 
       return TaskResult.defaultBuilder(request).build();
     }
 
   }
-  
+
   private RunTaskRequest buildRunTaskRequest(@Nonnull Config config) {
     RunTaskRequest request = null;
-    
-    
-    
+
+
+
     return request;
-}
+  }
 
 
 }
--- src/test/java/io/digdag/plugin/aws/ecs/EcsUtilsTest.java
+++ src/test/java/io/digdag/plugin/aws/ecs/EcsUtilsTest.java
@@ -1,13 +1,44 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package io.digdag.plugin.aws.ecs;
 
-import static org.junit.Assert.*;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
 import org.junit.Test;
+import com.amazonaws.services.ecs.model.RegisterTaskDefinitionRequest;
+import com.amazonaws.services.ecs.model.RunTaskRequest;
 
 public class EcsUtilsTest {
 
   @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void testRunTaskRequest() throws Exception {
+    String json = IOUtils.toString(EcsUtils.class.getResourceAsStream("run-task.json"), UTF_8);
+
+    RunTaskRequest request = EcsUtils.unmarshallRunTaskRequest(json);
+
+    Assert.assertEquals(0, request.getCount().intValue());
+    Assert.assertEquals("TASK_DEFINITION", request.getPropagateTags());
+  }
+
+  @Test
+  public void testRegisterTaskDefinitionRequest() throws Exception {
+    String json = IOUtils
+        .toString(EcsUtils.class.getResourceAsStream("register-task-definition.json"), UTF_8);
+
+    RegisterTaskDefinitionRequest request = EcsUtils.unmarshallRegisterTaskDefinitionRequest(json);
+
+    Assert.assertEquals("none", request.getNetworkMode());
+    Assert.assertEquals("task", request.getIpcMode());
   }
 
 }
