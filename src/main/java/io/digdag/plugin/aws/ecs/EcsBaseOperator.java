9beada4ea1aea416168a7b69b4a4305c0d3d4846
Makoto Yui <myui@apache.org>
2019-02-27 14:12:31 +0900

    Applied formatter

--- src/main/java/io/digdag/plugin/aws/ecs/EcsBaseOperator.java
+++ src/main/java/io/digdag/plugin/aws/ecs/EcsBaseOperator.java
@@ -1,18 +1,22 @@
 /*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package io.digdag.plugin.aws.ecs;
 
+import io.digdag.spi.OperatorContext;
+import io.digdag.spi.SecretProvider;
+import io.digdag.standards.operator.state.TaskState;
+import io.digdag.util.BaseOperator;
+import java.util.concurrent.ThreadLocalRandom;
+import javax.annotation.Nonnull;
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
@@ -22,74 +26,65 @@ import com.amazonaws.services.ecs.AmazonECSClientBuilder;
 import com.google.common.base.Optional;
 import com.google.common.base.Supplier;
 import com.google.common.io.BaseEncoding;
-import io.digdag.spi.OperatorContext;
-import io.digdag.spi.SecretProvider;
-import io.digdag.standards.operator.state.TaskState;
-import io.digdag.util.BaseOperator;
-
-import javax.annotation.Nonnull;
-import java.util.concurrent.ThreadLocalRandom;
 
 public abstract class EcsBaseOperator extends BaseOperator {
 
-    @Nonnull
-    protected final TaskState state;
+  @Nonnull
+  protected final TaskState state;
 
-    public EcsBaseOperator(OperatorContext context) {
-        super(context);
-        this.state = TaskState.of(request);
+  public EcsBaseOperator(OperatorContext context) {
+    super(context);
+    this.state = TaskState.of(request);
+  }
+
+  @Nonnull
+  protected AmazonECSClient getEcsClient() {
+    AWSCredentials credentials = credentials();
+    ClientConfiguration ecsClientConfiguration = new ClientConfiguration();
+
+    AmazonECSClientBuilder clientBuilder =
+        AmazonECSClient.builder().withCredentials(new AWSStaticCredentialsProvider(credentials))
+            .withClientConfiguration(ecsClientConfiguration);
+
+    return (AmazonECSClient) clientBuilder.build();
+  }
+
+  @Nonnull
+  protected AWSCredentials credentials() {
+    String tag = state.constant("tag", String.class, EcsBaseOperator::randomTag);
+    return credentials(tag);
+  }
+
+  @Nonnull
+  protected AWSCredentials credentials(@Nonnull String tag) {
+    SecretProvider awsSecrets = context.getSecrets().getSecrets("aws");
+    SecretProvider ecsSecrets = awsSecrets.getSecrets("ecs");
+
+    String accessKeyId = ecsSecrets.getSecretOptional("access_key_id")
+        .or(() -> awsSecrets.getSecret("access_key_id"));
+
+    String secretAccessKey = ecsSecrets.getSecretOptional("secret_access_key")
+        .or(() -> awsSecrets.getSecret("secret_access_key"));
+
+    return new BasicAWSCredentials(accessKeyId, secretAccessKey);
+  }
+
+  @Nonnull
+  protected static String randomTag() {
+    byte[] bytes = new byte[8];
+    ThreadLocalRandom.current().nextBytes(bytes);
+    return BaseEncoding.base32().omitPadding().encode(bytes);
+  }
+
+  @SafeVarargs
+  protected static <T> Optional<T> first(Supplier<Optional<T>>... suppliers) {
+    for (Supplier<Optional<T>> supplier : suppliers) {
+      Optional<T> optional = supplier.get();
+      if (optional.isPresent()) {
+        return optional;
+      }
     }
-
-    @Nonnull
-    protected AmazonECSClient getEcsClient() {
-        AWSCredentials credentials = credentials();
-        ClientConfiguration ecsClientConfiguration = new ClientConfiguration();
-
-        AmazonECSClientBuilder clientBuilder =
-                AmazonECSClient.builder()
-                        .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                        .withClientConfiguration(ecsClientConfiguration);
-
-        return (AmazonECSClient) clientBuilder.build();
-    }
-
-    @Nonnull
-    protected AWSCredentials credentials() {
-        String tag = state.constant("tag", String.class, EcsBaseOperator::randomTag);
-        return credentials(tag);
-    }
-
-    @Nonnull
-    protected AWSCredentials credentials(@Nonnull String tag) {
-
-        SecretProvider awsSecrets = context.getSecrets().getSecrets("aws");
-        SecretProvider ecsSecrets = awsSecrets.getSecrets("ecs");
-
-        String accessKeyId = ecsSecrets.getSecretOptional("access_key_id")
-                .or(() -> awsSecrets.getSecret("access_key_id"));
-
-        String secretAccessKey = ecsSecrets.getSecretOptional("secret_access_key")
-                .or(() -> awsSecrets.getSecret("secret_access_key"));
-
-        return new BasicAWSCredentials(accessKeyId, secretAccessKey);
-    }
-
-    @Nonnull
-    protected static String randomTag() {
-        byte[] bytes = new byte[8];
-        ThreadLocalRandom.current().nextBytes(bytes);
-        return BaseEncoding.base32().omitPadding().encode(bytes);
-    }
-
-    @SafeVarargs
-    protected static <T> Optional<T> first(Supplier<Optional<T>>... suppliers) {
-        for (Supplier<Optional<T>> supplier : suppliers) {
-            Optional<T> optional = supplier.get();
-            if (optional.isPresent()) {
-                return optional;
-            }
-        }
-        return Optional.absent();
-    }
+    return Optional.absent();
+  }
 
 }
--- src/main/java/io/digdag/plugin/aws/ecs/EcsPlugin.java
+++ src/main/java/io/digdag/plugin/aws/ecs/EcsPlugin.java
@@ -1,15 +1,13 @@
 /*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package io.digdag.plugin.aws.ecs;
 
@@ -17,29 +15,27 @@ import io.digdag.spi.OperatorFactory;
 import io.digdag.spi.OperatorProvider;
 import io.digdag.spi.Plugin;
 import io.digdag.spi.TemplateEngine;
-
 import java.util.Arrays;
 import java.util.List;
-
 import javax.inject.Inject;
 
 public class EcsPlugin implements Plugin {
+  @Override
+  public <T> Class<? extends T> getServiceProvider(Class<T> type) {
+    if (type == OperatorProvider.class) {
+      return EcsOperatorProvider.class.asSubclass(type);
+    } else {
+      return null;
+    }
+  }
+
+  public static class EcsOperatorProvider implements OperatorProvider {
+    @Inject
+    protected TemplateEngine templateEngine;
+
     @Override
-    public <T> Class<? extends T> getServiceProvider(Class<T> type) {
-        if (type == OperatorProvider.class) {
-            return EcsOperatorProvider.class.asSubclass(type);
-        } else {
-            return null;
-        }
+    public List<OperatorFactory> get() {
+      return Arrays.asList(new EcsRegisterOperatorFactory(templateEngine));
     }
-
-    public static class EcsOperatorProvider implements OperatorProvider {
-        @Inject
-        protected TemplateEngine templateEngine;
-
-        @Override
-        public List<OperatorFactory> get() {
-            return Arrays.asList(new EcsRegisterOperatorFactory(templateEngine));
-        }
-    }
+  }
 }
